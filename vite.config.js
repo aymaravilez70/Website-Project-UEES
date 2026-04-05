2e891f7b4cf81bbf41968f113943e4c7b8e2a202
Erick Roberto Arias Sánchez <erickarias1999@hotmail.com>
2026-04-05 18:51:14 -0500

    Adding initial website structure

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,5 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nosotros: resolve(__dirname, "nosotros.html"),
-        contacto: resolve(__dirname, "contacto.html"),
-      },
-    },
-  },
+  base: "/Website-Project-UEES/",
 });
